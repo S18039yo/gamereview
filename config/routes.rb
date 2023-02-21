deec1e566641cdb1160aecd783bf166ca1272c06
EC2 Default User <ec2-user@ip-172-31-17-5.ap-northeast-1.compute.internal>
2023-02-21 07:43:07 +0000

    ファイル編集

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,16 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   root to: 'homes#top'
+  get "/about" => "homes#about"
   
+  # 管理者用
   devise_for :admins, controllers: {
   sessions:      'admins/sessions',
-  passwords:     'admins/passwords',
-  registrations: 'admins/registrations'
   }
   
+  # 会員用
   devise_for :customers, controllers: {
   sessions:      'customers/sessions',
-  passwords:     'customers/passwords',
   registrations: 'customers/registrations'
   }
   
